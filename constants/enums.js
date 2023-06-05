0c5e5da042073244992285b48081dd3574647612
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-06-05 15:42:37 +0300

    create: constants for model

---
+++ constants/enums.js
@@ -0,0 +1,59 @@
+exports.GENDERS_ENUM = {
+  FEMALE: 'female',
+  MALE: 'male',
+};
+
+exports.COLORS_ENUM = {
+  BLACK: 'Black',
+  WHITE: 'White',
+  GRAY: 'Gray',
+  TABBY: 'Tabby',
+  ORANGE: 'Orange',
+  CALICO: 'Calico',
+  TUXEDO: 'Tuxedo',
+  SIAMESE: 'Siamese',
+  TORBIE: 'Torbie',
+  BROWN: 'Brown',
+  CREAM: 'Cream',
+  BLUE: 'Blue',
+  LILAC: 'Lilac',
+  FAWN: 'Fawn',
+  SILVER: 'Silver',
+  CINNAMON: 'Cinnamon',
+  CHOCOLATE: 'Chocolate',
+  LYNX: 'Lynx',
+  SMOKE: 'Smoke',
+  BICOLOR: 'Bicolor',
+};
+
+exports.CITY_ENUM = {
+  VINNYTSIA: 'Vinnytsia',
+  VOLYN: 'Volyn',
+  DNIPROPETROVSK: 'Dnipropetrovsk',
+  DONETSK: 'Donetsk',
+  ZHYTOMYR: 'Zhytomyr',
+  ZAKARPATTIA: 'Zakarpattia',
+  ZAPORIZHZHIA: 'Zaporizhzhia',
+  IVANOFRANKIVSK: 'Ivano-Frankivsk',
+  KYIV: 'Kyiv',
+  KIROVOHRAD: 'Kirovohrad',
+  LUHANSK: 'Luhansk',
+  LVIV: 'Lviv',
+  MYKOLAIV: 'Mykolaiv',
+  ODESSA: 'Odessa',
+  POLTAVA: 'Poltava',
+  RIVNE: 'Rivne',
+  SUMY: 'Sumy',
+  TERNOPIL: 'Ternopil',
+  KHARKIV: 'Kharkiv',
+  KHERSON: 'Kherson',
+  KHMELNYTSKYI: 'Khmelnytskyi',
+  CHERKASY: 'Cherkasy',
+  CHERNIHIV: 'Chernihiv',
+  CHERNIVTSI: 'Chernivtsi',
+};
+
+exports.STATUS_ENUM = {
+  DOMESTICATED: 'domesticated',
+  HOMELESS: 'homeless',
+};
