e12310c3e2a9eb1241f757954dcd35b99c0465de
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-06-05 15:49:57 +0300

    create: cat routes

---
+++ routes/api/cats.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const { cats: ctrl } = require('../../controllers');
+
+const router = express.Router();
+
+router.get('/', ctrl.getCatsList);
+router.post('/', ctrl.addNewCat);
+
+// router.get('/:catId', ctrl.getCat);
+// router.put('/:catId', ctrl.updateCat);
+// router.delete('/:catId', ctrl.deleteCat);
+
+// router.patch('/:catId/status', ctrl.updateCatStatus);
+
+module.exports = router;
