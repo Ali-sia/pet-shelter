d896da7cad812cdb1ae8a6e5a361b8c7c6028097
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-06-06 12:53:00 +0300

    update get cats list
    filter settings + pagination

--- controllers/cats/getCatsList.js
+++ controllers/cats/getCatsList.js
@@ -1,12 +1,53 @@
 const { Cat } = require('../../models/index');
 const { catchAsync } = require('../../utils');
 
+const { enums } = require('../../constants');
+
 const getCatsList = catchAsync(async (req, res, next) => {
-  const result = await Cat.find().sort({ createdAt: 1 }).lean();
+  const {
+    page,
+    limit,
+    status = 'all',
+    location = 'all',
+    gender = 'all',
+    color = 'all',
+  } = req.query;
+
+  ///api/cats?status=<homeless>&location=<Kherson>&color=<Orange>&gender=<female>&page=<1>&limit=<2>
+
+  const statusQuery =
+    status === 'all' ? enums.STATUS_ENUM.ALL.split(',') : status.split(',');
+  const locationQuery =
+    location === 'all' ? enums.CITY_ENUM.ALL.split(',') : location.split(',');
+  const colorQuery =
+    color === 'all' ? enums.COLORS_ENUM.ALL.split(',') : color.split(',');
+  const genderQuery =
+    gender === 'all' ? enums.GENDERS_ENUM.ALL.split(',') : gender.split(',');
+
+  const searchOptions = {
+    $and: [
+      { status: { $in: statusQuery } },
+      { location: { $in: locationQuery } },
+      { color: { $in: colorQuery } },
+      { gender: { $in: genderQuery } },
+    ],
+  };
+
+  const catsQuery = Cat.find(searchOptions);
+
+  const paginationPage = +page || 1;
+  const paginationLimit = +limit || 5;
+  const skip = (paginationPage - 1) * paginationLimit;
+
+  // const cats = await Todo.find().skip(skip).limit(paginationLimit);
+  catsQuery.skip(skip).limit(paginationLimit);
+  const catsCount = await Cat.count(catsQuery);
+  const result = await catsQuery;
 
   res.status(200).json({
     status: 'ok',
     code: 200,
+    total: catsCount,
     data: { result },
   });
 });
