f37ad16a3a9be609081a8daf9149c2b23cb07921
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-06-07 10:28:57 +0300

    create and import upload util

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,9 @@
 const catchAsync = require('./catchAsync');
 const catValidator = require('./CatValidation');
+const upload = require('./upload');
 
 module.exports = {
   catchAsync,
   catValidator,
+  upload,
 };
